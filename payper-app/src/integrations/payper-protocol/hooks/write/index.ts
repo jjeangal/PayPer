ae8da8874efb16a80dd0914f4c5358ae8434c368
efe-get-protocol <111286837+efe-get-protocol@users.noreply.github.com>
2023-10-18 01:35:25 +0200

    feat: notifications in test version

--- payper-app/src/integrations/payper-protocol/hooks/write/index.ts
+++ payper-app/src/integrations/payper-protocol/hooks/write/index.ts
@@ -2,10 +2,12 @@ import useBuyArticle from "./use-buy-article";
 import useCreateEdition from "./use-create-edition";
 import useCreateJournalist from "./use-create-journalist";
 import usePostArticle from "./use-post-article";
+import useSendNotification from "./use-send-notification";
 
 export {
   useBuyArticle,
   useCreateEdition,
   useCreateJournalist,
-  usePostArticle
+  usePostArticle,
+  useSendNotification
 }
