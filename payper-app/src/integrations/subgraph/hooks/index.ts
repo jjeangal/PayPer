958d29aac1d6bc09baf277b5199b81c473a43ac5
efe-get-protocol <111286837+efe-get-protocol@users.noreply.github.com>
2023-10-22 15:16:47 +0200

    subgraph hook

--- payper-app/src/integrations/subgraph/hooks/index.ts
+++ payper-app/src/integrations/subgraph/hooks/index.ts
@@ -2,10 +2,11 @@ import useGetArticleById from "./use-get-article-by-id";
 import useGetArticles from "./use-get-articles";
 import useGetArticlesFromJournalist from "./use-get-articles-from-journalist";
 import useGetJournalist from './use-get-journalist';
-
+import useGetPurchase from './use-get-purchase';
 export {
   useGetArticles,
   useGetArticlesFromJournalist,
   useGetJournalist,
-  useGetArticleById
+  useGetArticleById,
+  useGetPurchase
 }
